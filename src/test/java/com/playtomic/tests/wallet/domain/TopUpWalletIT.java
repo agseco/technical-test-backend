a5e0496fb982219bccd2886a7f5cf3f263e1698d
Alejandro Garcia Seco <alejandro.garcia@wallapop.com>
2025-03-03 20:20:26 +0100

    Use profile consistently in tests

--- src/test/java/com/playtomic/tests/wallet/domain/TopUpWalletIT.java
+++ src/test/java/com/playtomic/tests/wallet/domain/TopUpWalletIT.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -21,6 +22,7 @@ import static org.mockito.Mockito.doThrow;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class TopUpWalletIT {
 
     @Autowired
